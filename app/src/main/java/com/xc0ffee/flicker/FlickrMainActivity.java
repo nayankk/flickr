a5a183c0ca0109335a1d2c13ed301d2061cf7014
Nayan Kumar K <nayankk@gmail.com>
2016-11-02 10:39:55 -0700

    Initial code commit

---
+++ app/src/main/java/com/xc0ffee/flicker/FlickrMainActivity.java
@@ -0,0 +1,13 @@
+package com.xc0ffee.flicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlickrMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flickr_main);
+    }
+}
